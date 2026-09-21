d88660d7f8bc458a203a55ee293fe0d12146e4ee
robinsoncorrea4-boop <robinsoncorrea4@gmail.com>
2026-09-21 15:54:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,6 @@ except Exception as e:
 try:
     st.sidebar.image("logo_eficacia.png", use_container_width=True)
 except Exception:
-    st.sidebar.warning("🔍 Filtros de Visualización")
 
 st.sidebar.header("🔍 Filtros de Visualización")
 
